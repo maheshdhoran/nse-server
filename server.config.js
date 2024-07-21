97f8ac02934959475b0721ff3b79e39fc42f31fb
Mahesh Dhoran <maheshdhoran@Maheshs-MacBook-Pro.local>
2024-07-21 22:14:37 +0530

    removing pm2 log

--- server.config.js
+++ server.config.js
@@ -6,10 +6,6 @@ module.exports = {
       instances: 1,
       autorestart: true,
       watch: true, // Enable watching file changes
-      log_file: "logs/combined.log", // Log file
-      error_file: "logs/err.log", // Error log file
-      out_file: "logs/out.log", // Output log file
-      time: true, // Add timestamp in logs
       node_args: "--trace-warnings",
     },
   ],
